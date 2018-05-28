446cd7a643d5706348e09f4cc0174e86e937e268
Matthew Garrett <mjg59@google.com>
2018-05-28 10:06:33 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ import warnings
 
 dynamic_requires = []
 
-version = 0.6
+version = 0.7
 
 setup(
     name='lakeside',
-    version=0.6,
+    version=0.7,
     author='Matthew Garrett',
     author_email='mjg59@google.com',
     url='http://github.com/google/python-lakeside',
